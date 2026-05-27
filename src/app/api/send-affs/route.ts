d9c584299fb1e8b3a613d7868ee56b879508b242
kasi <danielkaser@mailbox.org>
2026-05-27 10:36:19 +0200

    create api routes for affs and affs-flirt-only

---
+++ src/app/api/send-affs/route.ts
@@ -0,0 +1,86 @@
+import { NextResponse, NextRequest } from "next/server";
+import { MongoClient } from "mongodb";
+import { marked } from "marked";
+import { transporter } from "@/utils";
+import {
+  apiAuthCheck,
+  getRandomDocument,
+  getRandomNumBetweenZeroAnd,
+} from "../utils";
+
+const { MONGODB_URI, EMAIL_SENDER, CRON_USERNAME, CRON_SECRET, NODE_ENV } =
+  process.env;
+
+const isDevMode = NODE_ENV === "development";
+
+if (!MONGODB_URI) throw new Error("No MONGODB_URI provided!");
+if (!CRON_USERNAME) throw new Error("No CRON_USERNAME provided!");
+if (!CRON_SECRET) throw new Error("No CRON_SECRET provided!");
+
+// MongoDB connection
+const client = new MongoClient(MONGODB_URI);
+let logMessage: string;
+
+export async function GET(request: NextRequest) {
+  // auth-check (only in production)
+  if (!isDevMode) apiAuthCheck(request, process.env);
+
+  // only execute about every 1/24 time (basically once per day, if API is called hourly)
+  const randomNum = getRandomNumBetweenZeroAnd(24);
+  if (randomNum !== 0) {
+    logMessage = `🟡 Zufallszahl ${randomNum}, nicht 0. Keine E-Mails gesendet.`;
+    console.log(logMessage);
+    return NextResponse.json({ message: logMessage }, { status: 204 });
+  }
+
+  try {
+    await client.connect();
+    const db = client.db("affs");
+    const tips = db.collection("data");
+
+    const tip = await getRandomDocument(tips);
+    const subscribers = await db.collection("subscribers").find().toArray();
+
+    if (!tip) {
+      logMessage = "❌ Keinen zufaelligen Tipp gefunden...";
+      console.log(logMessage);
+      return NextResponse.json({ message: logMessage }, { status: 404 });
+    }
+
+    const { content } = tip;
+    const subject = `Arschtritt vom Universum`;
+
+    // convert markdown to HTML
+    const html = {
+      content: await marked(content),
+    };
+
+    // send e-mails to all subscribers
+    for (const subscriber of subscribers) {
+      const emailBody = [html.content].join("");
+
+      await transporter.sendMail({
+        from: `Das Universum <${EMAIL_SENDER}>`,
+        to: subscriber.email,
+        subject,
+        html: emailBody,
+      });
+    }
+
+    logMessage = "✅ E-Mails erfolgreich gesendet!";
+
+    return NextResponse.json({
+      message: logMessage,
+      subject,
+      content: html.content,
+      numSubscribers: subscribers.length,
+      randomNum,
+    });
+  } catch (error) {
+    const { message } = error as Error;
+    console.log(message);
+    return NextResponse.json({ error: message }, { status: 500 });
+  } finally {
+    await client.close();
+  }
+}
